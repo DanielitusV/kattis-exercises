29a4ce2b3606d60414d130310380fcde9277ddb6
Daniel Virreira <virreira.daniel@gmail.com>
2025-06-25 15:15:39 -0400

    Agregar ejercicio tolvunarfraedingartelja.java

---
+++ Java/tolvunarfraedingartelja.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class tolvunarfraedingartelja {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int input = reader.nextInt();
+
+        int res = input - 1;
+        System.out.println(res);
+
+        reader.close();
+    }    
+}
