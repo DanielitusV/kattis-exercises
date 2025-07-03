b6aa311e2da01ff74b958a42af89a3429883ed54
Daniel_Virreira <virreira.daniel@gmail.com>
2025-07-02 23:37:06 -0400

    Agregar ejercicio bergmal

---
+++ Java/bergmal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class bergmal {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String input = reader.nextLine();
+
+        System.out.println(input);
+        reader.close();
+    }
+}
