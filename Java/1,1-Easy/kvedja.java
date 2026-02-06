e2cce14b3264f584830b86addc8261e25d7b43a4
DanielitusV <virreira.daniel@gmail.com>
2026-02-06 15:06:29 -0400

    Agregar ejercicio kvedja

---
+++ Java/1,1-Easy/kvedja.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class kvedja {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String input = reader.next();
+
+        System.out.println("Kvedja,");
+        System.out.println(input);
+    }
+}
