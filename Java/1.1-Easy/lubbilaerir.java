4fbc94bad95465e2913368173e51c6a9b3724664
DanielitusV <virreira.daniel@gmail.com>
2026-02-06 15:21:43 -0400

    Agregar ejercicio Lubbi Laerir

---
+++ Java/1.1-Easy/lubbilaerir.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class lubbilaerir {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String input = reader.next();
+
+        char first = input.charAt(0);
+        System.out.println(first);
+    }
+}
