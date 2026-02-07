fe6e927a4144c6e7950967448be179858bd75aba
DanielitusV <virreira.daniel@gmail.com>
2026-02-07 02:18:23 -0400

    Agregar ejercicio Add Two Numbers

---
+++ Java/1.2-Easy/addtwonumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class addtwonumbers {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int a = reader.nextInt();
+        int b = reader.nextInt();
+        int res = a + b;
+
+        System.out.println(res);
+    }
+}
