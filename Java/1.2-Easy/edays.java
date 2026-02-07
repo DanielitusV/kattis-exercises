8df6391f944654e3550da097fca328bd29b9a618
DanielitusV <virreira.daniel@gmail.com>
2026-02-07 02:52:44 -0400

    Agregar ejercicio EDays

---
+++ Java/1.2-Easy/edays.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner;
+
+public class edays {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        int miles = reader.nextInt();
+        int students = reader.nextInt();
+        int temperature = reader.nextInt();
+
+        System.out.println(miles * 2);
+    }
+}
